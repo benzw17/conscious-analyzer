408d24fa77fb86fd8449649db96f332866b1fb71
benzw17 <evergrow.today@gmail.com>
2021-02-18 11:04:21 +0100

    Version 1: Created a splash screen with transition to next activity.

--- app/src/main/java/com/example/consciousanalyzer/MainActivity.java
+++ app/src/main/java/com/example/consciousanalyzer/MainActivity.java
@@ -2,13 +2,66 @@ package com.example.consciousanalyzer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Global Variables
+
+    ImageView evergrowLogo;
+    private static int splash_screen = 3000;
+
+    //Animation variables
+    Animation topAnim, bottomAnim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // I added the following for hiding the task bar on top so it's really a welcome screen.
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        //Hooks
+        evergrowLogo = (ImageView) findViewById(R.id.evergrowLogo);
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+        //
+
+        //Animations now
+        evergrowLogo.setAnimation(topAnim);
+        //
+
+        //Create animation text for long tap on logo
+        evergrowLogo.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                Toast.makeText(MainActivity.this, "Welcome again", Toast.LENGTH_LONG).show();
+                return false;
+            }
+        });
+        //Done here
+
+        //Move away from first screen
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Dashboard.class);
+                startActivity(intent);
+                finish();
+            }
+        }, splash_screen);
+
+        //
+
     }
 }
