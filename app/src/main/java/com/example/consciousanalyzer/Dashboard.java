6c2274232565559366c2bec60342f2d4bc5c3820
benzw17 <evergrow.today@gmail.com>
2021-02-20 19:58:32 +0100

    Update #2: added to 2nd screen the functionality of giving a rating.

--- app/src/main/java/com/example/consciousanalyzer/Dashboard.java
+++ app/src/main/java/com/example/consciousanalyzer/Dashboard.java
@@ -2,13 +2,43 @@ package com.example.consciousanalyzer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
 public class Dashboard extends AppCompatActivity {
+
+    //variable declaration//
+    RatingBar ratingBar;
+    Button submitButton;
+    float numberRating;
+    int numberStars;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        final RatingBar ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+        Button submitButton = (Button) findViewById(R.id.submitButton);
+
+        ratingBar.setBackgroundColor(255);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int numberStars = ratingBar.getNumStars();
+                float numberRating = ratingBar.getRating();
+                String messageConfirmation = "You have given " + numberRating + " stars";
+                    Toast.makeText(Dashboard.this, messageConfirmation, Toast.LENGTH_SHORT).show();
+                }
+        });
+
+        }
+
     }
-}
--- app/src/main/java/com/example/consciousanalyzer/MainActivity.java
+++ app/src/main/java/com/example/consciousanalyzer/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
 public class MainActivity extends AppCompatActivity {
 
     //Global Variables
